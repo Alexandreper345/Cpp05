58f87bd010f9347fd3a2babb1abaf526205dc1f8
Alexandre da silva peroba <asilvaperoba@gmail.com>
2026-04-01 20:34:14 -0300

    feat: 🎸 process in ex01

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+private:
+	const std::string name;
+	int	grade;
+
+public:
+
+	std::string getname() const;
+	int getgrade() const;
+	void	increment(void);
+	void	decrement(void);
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &other);
+	~Bureaucrat();
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			GradeTooHighException() {}
+			virtual const char* what() const throw(){
+				return "Grade too high!";
+			}
+			virtual ~GradeTooHighException() throw() {}
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			GradeTooLowException() {}
+			const char* what() const throw(){
+				return "Grade too low!";
+			}
+			virtual ~GradeTooLowException() throw() {}
+	};
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
